89f2e8947ce7e25a43cd6a2df37104c0bbfdc735
Lisandre Begon <ubuntu@igtp6-port03.(none)>
2024-05-13 20:00:51 +0200

    Mise en place de la base de donnée entière

--- backend/controllers/indexControllers.js
+++ backend/controllers/indexControllers.js
@@ -1,12 +1,24 @@
 const {createUser} = require('../models/userModels');
+const {loginUser} = require('../models/userModels');
 
 exports.register = async (req, res) => {
-        try {
-            const data = req.body;
-            const userData = await createUser(data);
-            res.json(userData);
-        }
-        catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+    try {
+        const data = req.body;
+        const userData = await createUser(data);
+        res.json(userData);
     }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const data = req.body;
+        const user = await loginUser(data);
+        res.json({ message: "Login successful", user: user });
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -19,4 +19,33 @@ async function createUser(data) {
         throw new Error(`Error creating user' : ${error.message}`);
     }}
 
-module.exports = {createUser};
+async function loginUser(data) {
+    try {
+        // Fetch the user from the database
+        const user = await prisma.user.findUnique({
+            where: {
+                email: data.email
+            }
+        });
+
+        // If the user was not found, throw an error
+        if (!user) {
+            throw new Error('No user found with this email.');
+        }
+
+        // Compare the provided password with the stored hashed password
+        const passwordIsValid = bcrypt.compareSync(data.password, user.password);
+
+        // If the passwords do not match, throw an error
+        if (!passwordIsValid) {
+            throw new Error('Invalid password.');
+        }
+
+        // If the passwords match, the login is successful
+        return user;
+    } catch (error) {
+        throw new Error(`Error logging in: ${error.message}`);
+    }
+}
+
+module.exports = { createUser, loginUser };
